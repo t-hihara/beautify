302a953f0bfbfce154ab5899865dbd4fdad4d8cc
Roy <hey4phil@gmail.com>
2026-01-24 05:55:18 +0900

    guard設定

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,18 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+        'admin' => [
+            'driver' => 'session',
+            'provider' => 'users',
+        ],
+        'shop' => [
+            'driver' => 'session',
+            'provider' => 'users',
+        ],
+        'user' => [
+            'driver' => 'session',
+            'provider' => 'users',
+        ],
     ],
 
     /*
