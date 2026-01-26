eef1f9caf998a8b0d7c1845747c18928671fc7c0
Roy <hey4phil@gmail.com>
2026-01-26 23:25:56 +0900

    ログイン実装

---
+++ resources/manager/js/composables/useGuard.ts
@@ -0,0 +1,11 @@
+import { usePage } from "@inertiajs/vue3";
+import { computed } from "vue";
+
+export const useGuard = () => {
+  const page = usePage();
+  const guard = computed(() => page.props.guard as "admin" | "shop");
+
+  return {
+    guard,
+  };
+};
--- resources/manager/js/pages/Auth/Login.vue
+++ resources/manager/js/pages/Auth/Login.vue
@@ -1,24 +1,34 @@
 <script setup lang="ts">
 import { Head, useForm } from "@inertiajs/vue3";
+import { useGuard } from "@manager/composables/useGuard";
+import { computed } from "vue";
+import { route } from "ziggy-js";
 
 type LoginForm = {
   email: string;
   password: string;
 };
 
+const { guard } = useGuard();
+const isAdmin = computed((): boolean => guard.value === "admin");
+
 const form = useForm<LoginForm>({
   email: "",
   password: "",
 });
+
+const submit = (): void => form.post(route(`${guard.value}.login`));
 </script>
 
 <template>
-  <Head title="ログイン" />
+  <Head :title="isAdmin ? '管理者ログイン' : '店舗ログイン'" />
   <div class="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
     <div class="max-w-md w-full space-y-8">
-      <h2 class="mt-6 text-center text-3xl font-extrabold text-zinc-800">ログイン</h2>
+      <h2 class="mt-6 text-center text-3xl font-extrabold text-zinc-800">
+        {{ isAdmin ? "管理者ログイン" : "店舗ログイン" }}
+      </h2>
       <div class="mt-8">
-        <form>
+        <form @submit.prevent="submit">
           <div class="space-y-6">
             <div>
               <!-- form -->
@@ -30,7 +40,6 @@ const form = useForm<LoginForm>({
                     type="email"
                     name="email"
                     autocomplete="email"
-                    required
                     class="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-zinc-800 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                     placeholder="メールアドレス"
                   />
@@ -42,12 +51,16 @@ const form = useForm<LoginForm>({
                     type="password"
                     name="password"
                     autocomplete="password"
-                    required
                     class="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-zinc-800 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                    placeholder="メールアドレス"
+                    placeholder="password"
                   />
                 </div>
-                <div></div>
+              </div>
+              <div v-if="form.errors.email" class="mt-1">
+                <span class="text-sm text-red-500">{{ form.errors.email }}</span>
+              </div>
+              <div v-if="form.errors.password" class="mt-1">
+                <span class="text-sm text-red-500">{{ form.errors.password }}</span>
               </div>
             </div>
             <div>
