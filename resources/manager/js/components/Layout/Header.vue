870fe0f26a6846c54705f0ac9c98f96450f63c62
Roy <hey4phil@gmail.com>
2026-01-26 23:49:16 +0900

    ダッシュボードベース作成

---
+++ resources/manager/js/components/Layout/Header.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import type { User } from "@common/@types/inertia";
+import { route } from "ziggy-js";
+
+const { isAdmin } = defineProps<{
+  isAdmin: boolean;
+  user: User | null;
+}>();
+
+const submit = (): void => {
+  isAdmin ? route("admin.logout") : route("shop.logout");
+};
+</script>
+
+<template>
+  <header class="bg-white shadow-sm h-16">
+    <div class="flex justify-end items-center">
+      <template v-if="user">
+        <div class="flex items-center gap-4">
+          <span>{{ user.name }}</span>
+          <form @submit.prevent="submit">
+            <button type="submit" class="px-4 py-2 rounded-md bg-orange-400">ログアウト</button>
+          </form>
+        </div>
+      </template>
+    </div>
+  </header>
+</template>
+
+<style scoped></style>
--- resources/manager/js/layouts/Index.vue
+++ resources/manager/js/layouts/Index.vue
@@ -1,7 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { usePage } from "@inertiajs/vue3";
+import { computed } from "vue";
+import type { User } from "@common/@types/inertia";
+
+import Header from "@manager/components/Layout/Header.vue";
+
+const page = usePage();
+const user = computed<User | null>(() => page.props.auth.user || null);
+</script>
 
 <template>
   <div>
+    <Header :user="user" />
     <main>
       <slot></slot>
     </main>
