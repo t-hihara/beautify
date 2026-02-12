d751bb3c619c1d742cbf7b12c2a382c64bb80ac7
Roy <hey4phil@gmail.com>
2026-02-13 00:00:43 +0900

    登録処理修正

--- app/UseCases/Shop/CreateShopUseCase.php
+++ app/UseCases/Shop/CreateShopUseCase.php
@@ -15,17 +15,20 @@ class CreateShopUseCase
     public function __invoke(array $validated): ?Shop
     {
         return DB::transaction(function () use ($validated) {
-            $convert  = RecursiveCovert::_convert($validated, 'snake');
-            $shopData = $convert['shop'];
+            $convert   = RecursiveCovert::_convert($validated, 'snake');
+            $shopData  = $convert['shop'];
+            $imageDisk = config('filesystems.default');
+
 
             $this->shop->fill($shopData)->save();
             $this->shop->businessHours()->createMany($shopData['business_hours']);
 
-            foreach ($shopData['new_images'] as $newImage) {
-                $path = $newImage->store('shops/' . $this->shop->id, 's3');
+            foreach ($shopData['new_images'] ?? [] as $newImage) {
+                $path = $newImage->store('shops/' . $this->shop->id, $imageDisk);
                 $this->shop->images()->create([
+                    'disk'      => $imageDisk,
                     'file_path' => $path,
-                    'filename'  => $newImage->getClientOriginalName(),
+                    'file_name' => $newImage->getClientOriginalName(),
                     'mime_type' => $newImage->getMimeType(),
                     'file_size' => $newImage->getSize(),
                 ]);
