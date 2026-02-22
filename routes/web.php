4010eb91937849b47c5b0a811aa6e0d3edc1fc30
Roy <hey4phil@gmail.com>
2026-02-23 08:46:42 +0900

    testの削除

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\Public\HomeController;
 use App\Http\Controllers\Public\ShopController;
-use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(HomeController::class)->group(function () {
@@ -10,13 +9,6 @@ Route::controller(HomeController::class)->group(function () {
 });
 Route::controller(ShopController::class)->group(function () {
     Route::get('/shops', 'index')->name('shops.index');
-});
-
-Route::controller(TestController::class)->group(function () {
-    Route::get('/admin-test', 'adminIndex')->name('test.admin.index');
-    Route::get('/shop-test', 'shopIndex')->name('test.shop.index');
-    Route::get('/test', 'guestIndex')->name('test.index');
-    Route::get('/test_2', 'guestTransition')->name('test.transition');
 });
 
 require __DIR__ . '/admin.php';
