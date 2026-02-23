1bf3e604bf02848eae9dbc43ffcc56492753d792
Roy <hey4phil@gmail.com>
2026-02-23 10:32:54 +0900

    DB設計

---
+++ database/migrations/2026_02_23_003925_create_plans_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Enum\ActiveFlagTypeEnum;
+use App\Enum\PlanConditionTypeEnum;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('plans', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('shop_id')->constrained()->comment('店舗ID');
+            $table->string('name')->comment('プラン名');
+            $table->text('description')->comment('メニュー説明');
+            $table->unsignedInteger('total_duration')->comment('総所要時間');
+            $table->unsignedInteger('price')->comment('料金（税別）');
+            $table->string('discount_label')->nullable()->comment('割引ラベル');
+            $table->enum('condition_type', PlanConditionTypeEnum::cases())->nullable()->comment('適応条件種別');
+            $table->enum('active_flag', ActiveFlagTypeEnum::cases())->default('active')->comment('公開状態');
+            $table->unsignedInteger('sort_order')->comment('並び順');
+            $table->date('valid_from')->nullable()->comment('期間限定: 有効期間（開始）');
+            $table->date('valid_to')->nullable()->comment('期間限定: 有効期間（終了）');
+            $table->timestamps();
+            $table->index(['shop_id', 'active_flag', 'sort_order']);
+            $table->index(['shop_id', 'condition_type']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('plans');
+    }
+};
