cd781de36b7c3000af6db801265b9711ee890b01
Roy <hey4phil@gmail.com>
2026-02-25 01:43:57 +0900

    修正

--- database/migrations/0001_01_01_002007_create_menus_table.php
+++ database/migrations/0001_01_01_002007_create_menus_table.php
@@ -17,11 +17,11 @@ return new class extends Migration
             $table->id();
             $table->foreignId('shop_id')->constrained()->comment('店舗ID');
             $table->string('name')->comment('メニュー名');
-            $table->enum('type', MenuTypeEnum::cases())->comment('カテゴリ');
+            $table->enum('type', MenuTypeEnum::values())->comment('カテゴリ');
             $table->unsignedInteger('price')->comment('料金（税別）');
             $table->unsignedInteger('duration')->comment('所要時間');
             $table->text('description')->nullable()->comment('メニュー説明');
-            $table->enum('active_flag', ActiveFlagTypeEnum::cases())->comment('公開状態');
+            $table->enum('active_flag', ActiveFlagTypeEnum::values())->comment('公開状態');
             $table->unsignedInteger('sort_order')->comment('並び順');
             $table->timestamps();
         });
