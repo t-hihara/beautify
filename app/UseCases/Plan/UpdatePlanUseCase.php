a81510099c7033fd01224304dee51fcad1b415d7
Roy <hey4phil@gmail.com>
2026-02-25 01:01:53 +0900

    画像保存

--- app/UseCases/Plan/UpdatePlanUseCase.php
+++ app/UseCases/Plan/UpdatePlanUseCase.php
@@ -4,8 +4,10 @@ namespace App\UseCases\Plan;
 
 use App\Models\Plan;
 use App\Utilities\RecursiveCovert;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 
 class UpdatePlanUseCase
 {
@@ -14,11 +16,29 @@ class UpdatePlanUseCase
         $convert = RecursiveCovert::_convert($payload, 'snake');
 
         return DB::transaction(function () use ($convert, $plan) {
-            $planData = Arr::except($convert, ['image', 'menu_ids']);
-            $menuIds  = $convert['menu_ids'];
+            $planData  = Arr::except($convert, ['image', 'menu_ids']);
+            $imageData = $convert['image'] ?? null;
+            $menuIds   = $convert['menu_ids'];
 
             $plan->fill($planData)->save();
             $plan->menus()->sync($menuIds);
+
+            if ($imageData instanceof UploadedFile) {
+                $disk = config('filesystems.default');
+                $path = $imageData->store('plans/' . $plan->id, $disk);
+                $data = [
+                    'disk' => $disk,
+                    'file_path' => $path,
+                    'file_name' => $imageData->getClientOriginalName(),
+                    'mime_type' => $imageData->getMimeType(),
+                    'file_size' => $imageData->getSize(),
+                ];
+
+                if ($old = $plan->image) {
+                    Storage::disk($old->disk)->delete($old->file_path);
+                }
+                $plan->image()->updateOrCreate([], $data);
+            }
 
             return $plan;
         });
