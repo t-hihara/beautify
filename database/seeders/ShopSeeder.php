acf88bf32ccf23e88165ec33f1a53bb54bea23b9
Roy <hey4phil@gmail.com>
2026-02-27 01:42:20 +0900

    店舗にエリアと駅を持たせる

--- database/seeders/ShopSeeder.php
+++ database/seeders/ShopSeeder.php
@@ -3,10 +3,13 @@
 namespace Database\Seeders;
 
 use App\Enum\ActiveFlagTypeEnum;
+use App\Models\Area;
+use App\Models\Station;
 use Carbon\Carbon;
 use Faker\Factory;
 use Faker\Generator;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Seeder;
 
 class ShopSeeder extends BaseSeeder
@@ -24,6 +27,14 @@ class ShopSeeder extends BaseSeeder
     {
         $this->initialize();
 
+        $prefectures = [11, 12, 13, 14];
+        $areas = Area::whereIn('prefecture_id', $prefectures)
+            ->get()
+            ->groupBy('prefecture_id');
+        $stations = Station::whereIn('prefecture_id', $prefectures)
+            ->get()
+            ->groupBy('area_id');
+
         $items = [];
         $descriptions = [
             '当店はお客様に最高のサービスを提供いたします。',
@@ -33,15 +44,33 @@ class ShopSeeder extends BaseSeeder
         ];
 
         for ($i = 1; $i <= 200; $i++) {
+            $prefectureId = rand(1, 47);
+            $areaId       = null;
+            $stationId    = null;
+
+            if (in_array($prefectureId, $prefectures, true)) {
+                $prefectureAreas = $areas->get($prefectureId, collect());
+                if ($prefectureAreas->isNotEmpty()) {
+                    $areaId = $prefectureAreas->random()->id;
+
+                    $areaStations = $stations->get($areaId, collect());
+                    if ($areaStations->isNotEmpty()) {
+                        $stationId = $areaStations->random()->id;
+                    }
+                }
+            }
+
             $items[] = [
                 'name'          => $this->faker->company() . 'サロン',
                 'email'         => $this->faker->unique->safeEmail(),
                 'phone'         => $this->faker->phoneNumber(),
-                'prefecture_id' => rand(1, 47),
+                'prefecture_id' => $prefectureId,
                 'zipcode'       => str_replace('-', '', $this->faker->postcode()),
                 'address'       => $this->faker->city() . $this->faker->streetAddress(),
                 'building'      => $this->faker->boolean(60) ? $this->faker->buildingNumber() . ' ' . $this->faker->secondaryAddress() : null,
                 'description'   => $descriptions[array_rand($descriptions)],
+                'area_id'       => $areaId,
+                'station_id'    => $stationId,
                 'active_flag'   => $this->faker->boolean(60) ? ActiveFlagTypeEnum::ACTIVE : ActiveFlagTypeEnum::INACTIVE,
             ];
         }
