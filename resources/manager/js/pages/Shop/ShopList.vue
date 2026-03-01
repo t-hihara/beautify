aa250b861150c3d5831556105f7605d1f42ec924
Roy <hey4phil@gmail.com>
2026-03-01 17:36:45 +0900

    修正

--- resources/manager/js/pages/Shop/ShopList.vue
+++ resources/manager/js/pages/Shop/ShopList.vue
@@ -75,6 +75,7 @@ const { filters, shops } = defineProps<{
   shops: ShopType[];
   links: PaginationLinkType[];
   pagination: PaginationType;
+  errors?: Record<string, string>;
 }>();
 
 const searchForm = useForm<SearchFormType>("ShopListSearch", {
@@ -160,14 +161,22 @@ watch(
           :items="prefectures"
         />
       </div>
+      <div
+        v-if="errors && Object.keys(errors).length > 0"
+        class="inline-block mt-6 p-4 bg-red-100/50 rounded-lg border border-red-200"
+      >
+        <ul class="list-none list-inside space-y-1">
+          <li v-for="(error, key) in errors" :key="key" class="text-sm text-red-600">{{ error }}</li>
+        </ul>
+      </div>
     </div>
     <div class="mt-6 flex items-end justify-between">
       <search-single-select
         v-model="searchForm.perPage"
         title="表示件数"
         field="perPage"
-        :items="PER_PAGE_OPTIONS"
         class="max-w-28"
+        :items="PER_PAGE_OPTIONS"
       />
       <div class="flex items-center gap-2">
         <button-primary @click="exportFile('excel')" class="flex items-center gap-2"
