0048e95efd4eb90cf0c195b7dd2617123e9c29ee
Roy <hey4phil@gmail.com>
2026-03-03 22:59:47 +0900

    sanctumの導入

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,9 +4,14 @@ use App\Exceptions\InertiaValidationException;
 use App\Http\Middleware\Authenticate;
 use App\Http\Middleware\HandleInertiaRequests;
 use App\Http\Middleware\RedirectIfAuthenticated;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Routing\Middleware\SubstituteBindings;
+use Illuminate\Session\Middleware\StartSession;
+use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 use Spatie\Permission\Middleware\PermissionMiddleware;
 use Spatie\Permission\Middleware\RoleMiddleware;
 use Spatie\Permission\Middleware\RoleOrPermissionMiddleware;
@@ -14,12 +19,21 @@ use Spatie\Permission\Middleware\RoleOrPermissionMiddleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->statefulApi();
         $middleware->web(append: [
             HandleInertiaRequests::class,
+        ]);
+        $middleware->api(append: [
+            StartSession::class,
+            EncryptCookies::class,
+            AddQueuedCookiesToResponse::class,
+            EnsureFrontendRequestsAreStateful::class,
+            SubstituteBindings::class,
         ]);
         $middleware->alias([
             'guest'              => RedirectIfAuthenticated::class,
